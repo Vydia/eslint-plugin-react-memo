58feaf12389b5f685e3417b470ca35ed1a25470f
Paul Santos <paulmichael.a.santos@gmail.com>
2021-11-29 12:38:01 -0500

    updated the usememo fixer for jsx

--- __tests__/require-usecallback.ts
+++ __tests__/require-usecallback.ts
@@ -1,273 +1,274 @@
-import { RuleTester } from "eslint";
-import rule from "../require-usememo";
-import { normalizeIndent } from '../utils';
+// import { RuleTester } from "eslint";
+// import rule from "../require-usememo";
+// import { normalizeIndent } from '../utils';
 
-const ruleTester = new RuleTester({
-  parser: require.resolve("@typescript-eslint/parser"),
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-  },
-});
+// const ruleTester = new RuleTester({
+//   parser: require.resolve("@typescript-eslint/parser"),
+//   parserOptions: {
+//     ecmaFeatures: { jsx: true },
+//   },
+// });
 
-ruleTester.run("useCallback", rule, {
-  valid: [
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = useCallback(function() {}, []);
-          return <Child prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = useCallback(() => {}, []);
-          return <Child prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = function() {};
-          return <div prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = () => {};
-          return <div prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const myFn = () => {};
-        const Component = () => {
-          return <div prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn1 = useCallback(() => [], []);
-          const myFn2 = useCallback(() => myFn1, [myFn1]);
-          return <Child prop={myFn2} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = memoize(() => {});
-          return <Child prop={myFn} />;
-        }
-      `,
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = lodash.memoize(() => []);
-          return <Child prop={myFn} />;
-        }
-      `,
-    },
-  ],
-  invalid: [
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = function myFn() {};
-          return <Child prop={myFn} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          const myFn = useCallback(function() {}, []);
-          return <Child prop={myFn} />;
-        }
-      `,
-      errors: [{ messageId: "function-usecallback-props" }],
-    },
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = () => {};
-          return <Child prop={myFn} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          const myFn = useCallback(() => {}, []);
-          return <Child prop={myFn} />;
-        }
-      `,
-      errors: [{ messageId: "function-usecallback-props" }],
-    },
-    // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          let myFn = useCallback(() => ({}));
-          myFn = () => ({});
-          return <Child prop={myFn} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          let myFn = useCallback(() => ({}));
-          myFn = () => ({});
-          return <Child prop={myFn} />;
-        }
-      `,
-      errors: [{ messageId: "usememo-const" }],
-    },
-    // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          return <Child prop={() => {}} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          return <Child prop={() => {}} />;
-        }
-      `,
-      errors: [{ messageId: "function-usecallback-props" }],
-    },
-    // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          return <Child prop={() => []} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          return <Child prop={() => []} />;
-        }
-      `,
-      errors: [{ messageId: "function-usecallback-props" }],
-    },
-    // // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = memoize(() => {});
-          return <Child prop={myFn} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          const myFn = memoize(() => {});
-          return <Child prop={myFn} />;
-        }
-      `,
-      options: [{ strict: true }],
-      errors: [{ messageId: "unknown-usememo-props" }],
-    },
-    // // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn = lodash.memoize(() => []);
-          return <Child prop={myFn} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          const myFn = lodash.memoize(() => []);
-          return <Child prop={myFn} />;
-        }
-      `,
-      options: [{ strict: true }],
-      errors: [{ messageId: "unknown-usememo-props" }],
-    },
-    // // TODO: setup fixer for the following spec (output currently matches code)
-    {
-      code: normalizeIndent`
-        const Component = () => {
-          const myFn1 = () => [];
-          const myFn2 = useCallback(() => myFn1, [myFn1]);
-          return <Child prop={myFn2} />;
-        }
-      `,
-      output: normalizeIndent`
-        const Component = () => {
-          const myFn1 = () => [];
-          const myFn2 = useCallback(() => myFn1, [myFn1]);
-          return <Child prop={myFn2} />;
-        }
-      `,
-      errors: [{ messageId: "function-usecallback-deps" }],
-    },
-    {
-      code: normalizeIndent`
-        // @flow
-        import { d2Uri } from 'd2/utils/Routes'
-        import { prettyNumber } from '@vydia/js-core'
-        import { useHistory } from 'react-router'
-        import { useIsLabel } from 'd2/hooks/useIsLabel'
-        import { useOrganizationTotalConflictsBoxQuery, useUserTotalConflictsBoxQuery } from './queries'
-        import SummaryBox from 'd2/components/SummaryBox'
-        import useTranslations from './translations'
-        import type { StatelessFunctionalComponent } from 'react'
+// ruleTester.run("useCallback", rule, {
+//   valid: [
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = useCallback(function() {}, []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = useCallback(() => {}, []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = function() {};
+//           return <div prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = () => {};
+//           return <div prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const myFn = () => {};
+//         const Component = () => {
+//           return <div prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn1 = useCallback(() => [], []);
+//           const myFn2 = useCallback(() => myFn1, [myFn1]);
+//           return <Child prop={myFn2} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = memoize(() => {});
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = lodash.memoize(() => []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//     },
+//   ],
+//   invalid: [
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = function myFn() {};
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           const myFn = useCallback(function() {}, []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       errors: [{ messageId: "function-usecallback-props" }],
+//     },
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = () => {};
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           const myFn = useCallback(() => {}, []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       errors: [{ messageId: "function-usecallback-props" }],
+//     },
+//     // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           let myFn = useCallback(() => ({}));
+//           myFn = () => ({});
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           let myFn = useCallback(() => ({}));
+//           myFn = () => ({});
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       errors: [{ messageId: "usememo-const" }],
+//     },
+//     // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           return <Child prop={() => {}} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           return <Child prop={() => {}} />;
+//         }
+//       `,
+//       errors: [{ messageId: "function-usecallback-props" }],
+//     },
+//     // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           return <Child prop={() => []} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           return <Child prop={() => []} />;
+//         }
+//       `,
+//       errors: [{ messageId: "function-usecallback-props" }],
+//     },
+//     // // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = memoize(() => {});
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           const myFn = memoize(() => {});
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       options: [{ strict: true }],
+//       errors: [{ messageId: "unknown-usememo-props" }],
+//     },
+//     // // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn = lodash.memoize(() => []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           const myFn = lodash.memoize(() => []);
+//           return <Child prop={myFn} />;
+//         }
+//       `,
+//       options: [{ strict: true }],
+//       errors: [{ messageId: "unknown-usememo-props" }],
+//     },
+//     // // TODO: setup fixer for the following spec (output currently matches code)
+//     {
+//       code: normalizeIndent`
+//         const Component = () => {
+//           const myFn1 = () => [];
+//           const myFn2 = useCallback(() => myFn1, [myFn1]);
+//           return <Child prop={myFn2} />;
+//         }
+//       `,
+//       output: normalizeIndent`
+//         const Component = () => {
+//           const myFn1 = () => [];
+//           const myFn2 = useCallback(() => myFn1, [myFn1]);
+//           return <Child prop={myFn2} />;
+//         }
+//       `,
+//       errors: [{ messageId: "function-usecallback-deps" }],
+//     },
+//     {
+//       code: normalizeIndent`
+//         // @flow
+//         import { d2Uri } from 'd2/utils/Routes'
+//         import { prettyNumber } from '@vydia/js-core'
+//         import { useHistory } from 'react-router'
+//         import { useIsLabel } from 'd2/hooks/useIsLabel'
+//         import { useOrganizationTotalConflictsBoxQuery, useUserTotalConflictsBoxQuery } from './queries'
+//         import SummaryBox from 'd2/components/SummaryBox'
+//         import useTranslations from './translations'
+//         import type { StatelessFunctionalComponent } from 'react'
 
-        const TotalConflictsBox: StatelessFunctionalComponent<{}> = () => {
-          const [isLabel] = useIsLabel()
-          const [organizationData] = useOrganizationTotalConflictsBoxQuery({ isLabel })
-          const [userData] = useUserTotalConflictsBoxQuery({ isLabel })
-          const t = useTranslations()
-          const history = useHistory()
+//         const TotalConflictsBox: StatelessFunctionalComponent<{}> = () => {
+//           const [isLabel] = useIsLabel()
+//           const [organizationData] = useOrganizationTotalConflictsBoxQuery({ isLabel })
+//           const [userData] = useUserTotalConflictsBoxQuery({ isLabel })
+//           const t = useTranslations()
+//           const history = useHistory()
 
-          const data = isLabel ? organizationData : userData
-          return (<SummaryBox
-            icon='ban'
-            onClick={() => history.push(d2Uri('/insights/conflicts'))}
-            testID='WidgetDashboard-TotalConflictsBox'
-            title={isLabel ? t.labelTotalConflicts : t.userTotalConflicts}
-            value={data && prettyNumber(data.totalConflicts)}
-          />)
-        }
+//           const data = isLabel ? organizationData : userData
+//           return (<SummaryBox
+//             icon='ban'
+//             onClick={() => history.push(d2Uri('/insights/conflicts'))}
+//             testID='WidgetDashboard-TotalConflictsBox'
+//             title={isLabel ? t.labelTotalConflicts : t.userTotalConflicts}
+//             value={data && prettyNumber(data.totalConflicts)}
+//           />)
+//         }
 
-        export default TotalConflictsBox
-      `,
-      output: normalizeIndent`
-        // @flow
-        import { d2Uri } from 'd2/utils/Routes'
-        import { prettyNumber } from '@vydia/js-core'
-        import { useHistory } from 'react-router'
-        import { useIsLabel } from 'd2/hooks/useIsLabel'
-        import { useOrganizationTotalConflictsBoxQuery, useUserTotalConflictsBoxQuery } from './queries'
-        import SummaryBox from 'd2/components/SummaryBox'
-        import useTranslations from './translations'
-        import type { StatelessFunctionalComponent } from 'react'
+//         export default TotalConflictsBox
+//       `,
+//       output: normalizeIndent`
+//         // @flow
+//         import { d2Uri } from 'd2/utils/Routes'
+//         import { prettyNumber } from '@vydia/js-core'
+//         import { useHistory } from 'react-router'
+//         import { useIsLabel } from 'd2/hooks/useIsLabel'
+//         import { useOrganizationTotalConflictsBoxQuery, useUserTotalConflictsBoxQuery } from './queries'
+//         import SummaryBox from 'd2/components/SummaryBox'
+//         import useTranslations from './translations'
+//         import type { StatelessFunctionalComponent } from 'react'
 
-        const TotalConflictsBox: StatelessFunctionalComponent<{}> = () => {
-          const [isLabel] = useIsLabel()
-          const [organizationData] = useOrganizationTotalConflictsBoxQuery({ isLabel })
-          const [userData] = useUserTotalConflictsBoxQuery({ isLabel })
-          const t = useTranslations()
-          const history = useHistory()
+//         const TotalConflictsBox: StatelessFunctionalComponent<{}> = () => {
+//           const [isLabel] = useIsLabel()
+//           const [organizationData] = useOrganizationTotalConflictsBoxQuery({ isLabel })
+//           const [userData] = useUserTotalConflictsBoxQuery({ isLabel })
+//           const t = useTranslations()
+//           const history = useHistory()
+//           const onClickConflicts = useCallback(() => history.push(d2Uri('/insights/conflicts')), [history])
 
-          const data = isLabel ? organizationData : userData
-          return (<SummaryBox
-            icon='ban'
-            onClick={() => history.push(d2Uri('/insights/conflicts'))}
-            testID='WidgetDashboard-TotalConflictsBox'
-            title={isLabel ? t.labelTotalConflicts : t.userTotalConflicts}
-            value={data && prettyNumber(data.totalConflicts)}
-          />)
-        }
+//           const data = isLabel ? organizationData : userData
+//           return (<SummaryBox
+//             icon='ban'
+//             onClick={onClickConflicts}
+//             testID='WidgetDashboard-TotalConflictsBox'
+//             title={isLabel ? t.labelTotalConflicts : t.userTotalConflicts}
+//             value={data && prettyNumber(data.totalConflicts)}
+//           />)
+//         }
 
-        export default TotalConflictsBox
-      `,
-      errors: [{ messageId: "function-usecallback-props"}]
-    }
-  ],
-});
+//         export default TotalConflictsBox
+//       `,
+//       errors: [{ messageId: "function-usecallback-props"}]
+//     }
+//   ],
+// });
--- require-usememo.ts
+++ require-usememo.ts
@@ -9,6 +9,8 @@ import {
   isComplexComponent,
   MemoStatus,
 } from "./common";
+import { fromPairs } from "lodash";
+import { isConstructorDeclaration } from "typescript";
 
 const hookNameRegex = /^use[A-Z0-9].*$/;
 
@@ -69,6 +71,7 @@ const rule: Rule.RuleModule = {
     ],
   },
   create: (context) => {
+    let alreadyFixedJsx: boolean = false
     function report(node: Rule.Node, messageId: keyof typeof messages) {
       context.report({ node, messageId: messageId as string });
     }
@@ -83,6 +86,73 @@ const rule: Rule.RuleModule = {
       const [name, value] = sourceCode.getText(definition).split(/=(.+)/)
       const fixedCode = `${name.trim()} = useMemo(() => ${implicitReturn ? value.trim() : `(${value.trim()})`}, [])`
       return fixer.replaceText(definition, fixedCode)
+    }
+
+    const useMemoJsxFix = (fixer: Rule.RuleFixer, node: Rule.Node,  implicitReturn: boolean = true): Rule.Fix | null => {
+      // console.log(fixer)
+      // console.log(context.options)
+      if (alreadyFixedJsx) {
+        alreadyFixedJsx = false
+        return null
+      }
+      const sourceCode = context.getSourceCode();
+      const references = context.getScope()?.references
+      // const returnStatement = references?[0]
+      // @ts-ignore
+      const block = context.getScope().block.body.body
+      // console.log(context.getScope().block)
+      if (!block) return null
+      const returnLine = block[block.length - 1].loc.start.line
+      const lineAboveReturn = sourceCode.getIndexFromLoc({ line: returnLine, column: 0 })
+// @ts-ignore
+      const filteredRefs = references?.filter((reference) => reference?.identifier.type === 'Identifier')
+// @ts-ignore
+      // console.log(filteredRefs)
+      // @ts-ignore
+      // console.log(filteredRefs[0].identifier.parent.parent.parent.parent.parent)
+      // @ts-ignore
+      // const parentAttributes = filteredRefs[0].identifier.parent.attributes
+      // @ts-ignore
+      // const filteredJsxExp = parentAttributes.filter((ref) => ref.value.type === 'JSXExpressionContainer')
+      // @ts-ignore
+      // console.log(filteredJsxExp)
+      // console.log(sourceCode.getText(references[1].identifier))
+      // console.log(sourceCode.getText(filteredRefs[0].identifier.parent).split('.'))
+      // console.warn(node)
+      // console.warn('node' ,sourceCode.getText(node))
+      // @ts-ignore
+      const [t, nameOfConst] = sourceCode.getText(filteredRefs[0].identifier.parent).split('.')
+      // @ts-ignore
+      const nodeText = sourceCode.getText(node)
+      // const [name, value] = sourceCode.getText(node).split(/[^\w*=](.*)/)
+      // console.log(name)
+      const value = nodeText.replace(/^\w*=/, '')
+      // @ts-ignore
+      const name = nodeText.match(/^\w*/)[0]
+      console.log(name)
+      // @ts-ignore
+      const nameOfConst = `${name}${node.start}`
+      console.log(nameOfConst)
+      // const definition = filteredRefs?.[0]?.identifier.parent
+      // if (!definition) return null
+      // const [name, value] = sourceCode.getText(definition).split(/=(.+)/)
+      // const fixedCode = `${name.trim()} = useMemo(() => ${implicitReturn ? value.trim() : `(${value.trim()})`}, [])`
+      const replacedParens = value.replace(/^{/, '(').replace(/\}$/, ')')
+      const toInsert = `const ${nameOfConst} = useMemo(() => ${replacedParens}, [])\n`
+      // console.log(filteredRefs[0].identifier.parent.parent.parent.parent.parent)
+      // fixer.insertTextAfterRange([lineAboveReturn, lineAboveReturn], toInsert)
+      // fixer.replaceText(filteredRefs[0].identifier.parent.parent.parent.parent.parent, `${name}={${nameOfConst}}`)
+      // return null
+      // console.log(toInsert)
+      // alreadyFixedJsx = true
+// @ts-ignore
+      // const identifier = filteredRefs[0].identifier.parent.parent.parent.parent.parent
+      // console.log(identifier)
+      // @ts-ignore
+      return [
+        fixer.insertTextAfterRange([lineAboveReturn, lineAboveReturn], toInsert),
+        fixer.replaceText(node, `${name}={${nameOfConst}}`),
+      ].filter(i => i)
     }
 
     const useCallbackFix = (fixer: Rule.RuleFixer): Rule.Fix | null => {
@@ -112,26 +182,27 @@ const rule: Rule.RuleModule = {
           const { expression } = value;
           if (expression?.type !== "JSXEmptyExpression") {
             switch (getExpressionMemoStatus(context, expression)) {
-              case MemoStatus.UnmemoizedObject:
-                context.report({ node, messageId: "object-usememo-props", fix: (fixer) => useMemoFix(fixer, false) });
-                break;
-              case MemoStatus.UnmemoizedArray:
-                context.report({ node, messageId: "array-usememo-props", fix: (fixer) => useMemoFix(fixer) });
-                break;
-              case MemoStatus.UnmemoizedNew:
-                context.report({ node, messageId: "instance-usememo-props", fix: (fixer) => useMemoFix(fixer) });
-                break;
-              case MemoStatus.UnmemoizedFunction:
-                context.report({ node, messageId: "function-usecallback-props", fix: useCallbackFix });
-                break;
-              case MemoStatus.UnmemoizedFunctionCall:
-              case MemoStatus.UnmemoizedOther:
-                if (context.options?.[0]?.strict) {
-                  report(node, "unknown-usememo-props");
-                }
-                break;
+              // case MemoStatus.UnmemoizedObject:
+              //   context.report({ node, messageId: "object-usememo-props", fix: (fixer) => useMemoFix(fixer, false) });
+              //   break;
+              // case MemoStatus.UnmemoizedArray:
+              //   context.report({ node, messageId: "array-usememo-props", fix: (fixer) => useMemoFix(fixer) });
+              //   break;
+              // case MemoStatus.UnmemoizedNew:
+              //   context.report({ node, messageId: "instance-usememo-props", fix: (fixer) => useMemoFix(fixer) });
+              //   break;
+              // case MemoStatus.UnmemoizedFunction:
+              //   context.report({ node, messageId: "function-usecallback-props", fix: useCallbackFix });
+              //   break;
+              // case MemoStatus.UnmemoizedFunctionCall:
+              // case MemoStatus.UnmemoizedOther:
+              //   if (context.options?.[0]?.strict) {
+              //     report(node, "unknown-usememo-props");
+              //   }
+              //   break;
               case MemoStatus.UnmemoizedJSX:
-                report(node, "jsx-usememo-props");
+                // console.log('sup', node)
+                context.report({ node, messageId: "jsx-usememo-props", fix: (fixer) => useMemoJsxFix(fixer, node) });
                 break;
             }
           }
